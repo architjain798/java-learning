20f38dee8521f4a7d5bc3461dca53527f2ed4255
Archit Jain <architjain798@gamil.com>
2024-11-24 00:18:05 +0530

    Add the function

--- Jshell/src/Main.java
+++ Jshell/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("This is a function call.");
     }
 }
