6f0fa1145177184b3f02af3cae39cbbcdf4487d3
Archit Jain <architjain798@gmail.com>
2024-12-01 00:20:57 +0530

    Add Planet class with main method and resolve function

---
+++ Jshell/src/Planet.java
@@ -0,0 +1,10 @@
+class Planet {
+    public static void main(String [] args){
+        Planet obj = new Planet();
+        obj.resolve();
+    }
+
+    void resolve(){
+        System.err.println("Resolve");
+    }
+}
