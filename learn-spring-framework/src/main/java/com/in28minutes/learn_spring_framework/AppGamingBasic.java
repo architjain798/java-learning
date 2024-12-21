5f11ed724496589d601a99118b069efb9f80e2b4
Archit Jain <architjain798@gmail.com>
2024-12-21 17:13:00 +0530

    Tightly coupiling example

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
@@ -2,11 +2,15 @@ package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasic {
+
     public static void main(String[] args) {
         var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
+        var superContraGame = new SuperContraGame();
+        // var gameRunner = new GameRunner(marioGame);
+        var gameRunner = new GameRunner(superContraGame);
         gameRunner.runGame();
     }
 }
