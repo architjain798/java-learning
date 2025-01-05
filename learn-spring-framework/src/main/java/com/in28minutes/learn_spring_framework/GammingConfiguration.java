0910b06e9214ff17d0b4d139faa73c806006791c
Archit Jain <architjain798@gmail.com>
2025-01-05 15:11:52 +0530

    Add App03GamingSpringBeans and GammingConfiguration for game management

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/GammingConfiguration.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learn_spring_framework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import com.in28minutes.learn_spring_framework.game.PacManGame;
+
+@Configuration
+public class GammingConfiguration {
+
+    @Bean
+    GamingConsole game() {
+        return new PacManGame();
+    }
+
+    @Bean
+    GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+
+}
