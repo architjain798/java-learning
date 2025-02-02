e7af4708844d3b69c49eb6da4e42cf2bfbf163e8
Archit Jain <architjain798@gmail.com>
2025-02-02 18:43:52 +0530

    Remove @Qualifier from GameRunner constructor and add @Primary annotation to PacManGame for improved dependency resolution

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,13 +1,12 @@
 package com.in28minutes.learn_spring_framework.game;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
     private GamingConsole game;
 
-    public GameRunner(@Qualifier("superContraGameQualifier") GamingConsole game) {
+    public GameRunner(GamingConsole game) {
         this.game = game;
     }
 
--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/PacManGame.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/PacManGame.java
@@ -1,8 +1,10 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class PacManGame implements GamingConsole {
 
     @Override
