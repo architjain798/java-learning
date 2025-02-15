69e62f47c7f6ebc8f822ff75d0320d3fbb9ab7a4
Archit Jain <architjain798@gmail.com>
2025-02-15 19:51:39 +0530

    Add comment to clarify eager initialization in SingletonDesignPattern class

--- design_pattern/src/com/design/pattern/singleton/SingletonDesignPattern.java
+++ design_pattern/src/com/design/pattern/singleton/SingletonDesignPattern.java
@@ -2,6 +2,7 @@ package com.design.pattern.singleton;
 
 public class SingletonDesignPattern {
     // The object will be loaded at class loading only even without calling the get instance
+    // Eager Initialization
     static SingletonDesignPattern obj = new SingletonDesignPattern();
     
     private SingletonDesignPattern(){}
