eddd557f07dec60c2240c98807a33ec0454dfaf1
Archit Jain <architjain798@gmail.com>
2025-03-29 16:39:25 +0530

    feat: add World class for multithreading demonstration and update main method in Test class

--- multithreading/src/com/practice/Test.java
+++ multithreading/src/com/practice/Test.java
@@ -5,5 +5,12 @@ public class Test {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println(Thread.currentThread().getName());
+
+        World world = new World();
+        world.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
     }
 }
