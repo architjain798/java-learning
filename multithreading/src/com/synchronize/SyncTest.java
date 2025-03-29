68899cf035979e13cdf5b30ecec117242cfb04c4
Archit Jain <architjain798@gmail.com>
2025-03-29 23:41:43 +0530

    feat: synchronize increment method in Counter class to prevent race conditions

--- multithreading/src/com/synchronize/SyncTest.java
+++ multithreading/src/com/synchronize/SyncTest.java
@@ -26,8 +26,16 @@ class Counter {
         this.count = 0;
     }
 
-    public void increment() {
+    public synchronized void increment() {
         count++;
+
+        // this section is critical section
+    }
+
+    public void incrementNew() {
+        synchronized (this) {
+            count++;
+        }
     }
 
     public int getCount() {
@@ -39,8 +47,8 @@ public class SyncTest {
 
     public static void main(String[] args) throws InterruptedException {
         Counter c1 = new Counter();
-        MyThreadSync t1 = new MyThreadSync(c1,"t-1");
-        MyThreadSync t2 = new MyThreadSync(c1,"t-2");
+        MyThreadSync t1 = new MyThreadSync(c1, "t-1");
+        MyThreadSync t2 = new MyThreadSync(c1, "t-2");
 
         t1.start();
         t2.start();
@@ -51,3 +59,10 @@ public class SyncTest {
         System.out.println(c1.getCount());
     }
 }
+
+// when we were not using synchronized the multiple thread were trying to update the value at same time
+// due to this the result were unpredicatable
+// this is called RACE CONDITION
+
+// WHEN WE APPLY synchronized keyword to achieve the MUTUAL EXCULSION
+// it ensures that multiple thread do not access the critical section simultaneosuly
