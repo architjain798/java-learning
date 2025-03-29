caabd2382baba8b4dc22150b7162f6a04d37eae7
Archit Jain <architjain798@gmail.com>
2025-03-29 17:03:55 +0530

    feat: update Test class with thread lifecycle comments and clarify thread states

--- multithreading/src/com/practice/Test.java
+++ multithreading/src/com/practice/Test.java
@@ -7,8 +7,8 @@ public class Test {
         System.out.println(Thread.currentThread().getName());
 
         // extending thread class
-        World world = new World();
-        world.start();
+        World world = new World();  // NEW STATE
+        world.start(); // RUNNABLE STATE
 
         // implememting runnable interface
         WorldModern worldModern = new WorldModern();
@@ -21,3 +21,16 @@ public class Test {
         }
     }
 }
+// Thread LifeCycle
+// New: A thread is in this state when it is created but not
+// yet started.
+// · Runnable: After the start method is called, the thread
+// becomes runnable. It's ready to run and is waiting for
+// CPU time.
+// . Running: The thread is in this state when it is executing.
+// · Blocked/Waiting: A thread is in this state when it is
+// waiting for a resource or for another thread to perform
+// an action.
+// · Terminated: A thread is in this state when it has
+// finished executing.
+
