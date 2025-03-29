d52d6ac651082261c4a45362594654988fc8b0a9
Archit Jain <architjain798@gmail.com>
2025-03-29 16:43:29 +0530

    feat: update Test class to demonstrate extending Thread and implementing Runnable for multithreading

--- multithreading/src/com/practice/Test.java
+++ multithreading/src/com/practice/Test.java
@@ -6,13 +6,16 @@ public class Test {
         System.out.println("Hello, World!");
         System.out.println(Thread.currentThread().getName());
 
+        // extending thread class
         World world = new World();
         world.start();
 
+        // implememting runnable interface
         WorldModern worldModern = new WorldModern();
         Thread t1 = new Thread(worldModern);
         t1.start();
 
+        // main thread it is
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName());
         }
