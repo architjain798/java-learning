3d17b57638de3bee7b2c17e578f0992cb68a290b
Archit Jain <architjain798@gmail.com>
2025-03-31 16:51:30 +0530

    feat: implement ReadWriteCounter class with read-write locks for thread-safe increment and retrieval

--- multithreading/src/com/locks/ReadWriteCounter.java
+++ multithreading/src/com/locks/ReadWriteCounter.java
@@ -1,5 +1,73 @@
 package com.locks;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class ReadWriteCounter {
-    
+
+    private int count = 0;
+
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private final Lock readLock = lock.readLock();
+
+    private final Lock writeLock = lock.writeLock();
+
+    public void incremenet() {
+        writeLock.lock();
+        try {
+            count++;
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public int getCount() {
+        readLock.lock();
+        try {
+            return count;
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReadWriteCounter counter = new ReadWriteCounter();
+
+        Runnable readTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " read: " + counter.getCount());
+                }
+            }
+
+        };
+
+        Runnable writeTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    counter.incremenet();
+                    System.out.println(Thread.currentThread().getName() + " incremented! ");
+                }
+            }
+
+        };
+
+        Thread writerThread = new Thread(writeTask, "writerThread");
+        Thread readerThread = new Thread(readTask, "readerThread 1");
+        Thread readerThread2 = new Thread(readTask, "readerThread 2");
+
+        readerThread2.start();
+        readerThread.start();
+        writerThread.start();
+
+    }
+
 }
+// jab read thread read kr rha hoga
+// write lock read krne ka wait krega
+// vo lock release krega
+// tab ki write hoga vrna read aur write inconsisnent ho jaenge
