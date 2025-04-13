dd06603e0ab19c03b4cdbf46f9714e80fa30ea0e
Archit Jain <architjain798@gmail.com>
2025-04-13 18:22:24 +0530

    feat: add JavaImport class with main method demonstrating string output and collection sorting

---
+++ java-tips/src/com/javaimport/JavaImport.java
@@ -0,0 +1,23 @@
+package com.javaimport;
+
+import static java.lang.System.out;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import static java.util.Collections.*;
+
+public class JavaImport {
+
+    public static void main(String[] args) {
+        String str = "archit";
+        out.println(str);
+
+        BigDecimal db = null;
+        out.println(db);
+
+        sort(new ArrayList<String>());
+
+    }
+}
+
+// by default  java.lang package is there
+
